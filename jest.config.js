61e33aa9f9175a879d45f91d173deb5fcdd3365a
Rendy Artha <rendyartha312@gmail.com>
2021-09-09 22:56:20 +0700

    setup jest cypress testing-library and github action

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  ...require("./test/jest-common"),
+  collectCoverageFrom: [
+    "./(components|etc|lib|pages)/**/*.(ts|tsx|js|jsx)",
+    "!./(components|etc|lib|pages)/**/__tests__/**/*.test.(ts|tsx|js|jsx)",
+    "!./(components|etc|lib|pages)/**/__mocks__/**/*.(ts|tsx|js|jsx)",
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 83,
+      branches: 74,
+      functions: 81,
+      lines: 83,
+    },
+  },
+  transform: {
+    "^.+\\.(ts|tsx|js|jsx)$": "babel-jest",
+  },
+  projects: ["./test/jest.client.js", "./test/jest.server.js"],
+};
